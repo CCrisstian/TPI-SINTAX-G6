4ef3aafca951d10bc25903c849efaf277b2c0081
CCrisstian <crisstiann.c@gmail.com>
2023-06-23 17:04:14 -0300

    parser - GRAMATICA - falta LINK, IMAGENDATA Y VIDEODATA

--- parser_1.py
+++ parser_1.py
@@ -84,5 +84,161 @@ def p_copyright(p):
 def p_genTitle(p):
     '''genTitle : texto | emphasis | link | email | texto genTitle | emphasis genTitle | link genTitle | email genTitle'''
 
+def p_title(p):
+    '''title : OPTIT genTitle CLTIT'''
+
+def p_genSECL(p):
+    '''genSECL : texto | emphasis | link | email | author | comment | texto genSECL | emphasis genSECL | link genSECL 
+    | email  genSECL| author genSECL| comment genSECL'''
+
+def p_simPara(p):
+    '''simPara : OPSPARA genSECL CLSPARA'''
+
+def p_emphasis(p):
+    '''emphasis : OPEMPH genSECL CLEMPH'''
+
+def p_comment(p):
+    '''comment : OPCOM genSECL CLCOM'''
+
+def p_link(p):
+    '''link :  genSECL link | genSECL'''
+ 
+def p_genPara(p):
+    '''genPara : texto | emphasis | link | email | author | comment | itemizedList | important | address | mediaObject 
+    | informalTable | texto genPara | emphasis genPara | link genPara | email genPara | author genPara | comment genPara 
+    | itemizedList genPara | important genPara | address genPara | mediaObject genPara | informalTable genPara'''
+
+def p_para(p):
+    '''para : OPPARA genPara CLPARA'''
+
+def p_important(p):
+    '''important : title genArt | genArt'''
+
+def p_genFC(p):
+    '''genFC : texto | link | emphasis | comment | texto genFC | link genFC | emphasis genFC | comment genFC '''
+
+def p_firstName(p):
+    '''firstName : OPFNAME genFC CLFNAME'''
+
+def p_surname(p):
+    '''surname : OPSNAME genFC CLSMANE'''
+
+def p_street(p):
+    '''street : OPSTREET genFC CLSTREET'''
+
+def p_city(p):
+    '''city : OPCITY genFC CLCITY'''
+
+def p_state(p):
+    '''state → OPSTATE genFC CLSTATE'''
+
+def p_phone(p):
+    '''Phone → OPPHONE genFC CLPHONE'''
+
+def p_email(p):
+    '''email : OPEMAIL genFC CLEMAIL'''
+
+def p_date(p):
+    '''date : OPDATE genFC CLDATE'''
+
+def p_year(p):
+    '''year : OPYEAR genFC CLYEAR'''
+
+def p_holder(p):
+    '''holder : OPHOLDER genFC CLHOLDER'''
+
+def p_genMediaObj(p):
+    '''genMediaObj : videoObject | imageObject | videoObject genMediaObj | imageObject genMediaObj'''
+
+def p_mediaobject(p):
+    '''mediaobject : OPMOBJ info videoObject CLMOBJ | OPMOBJ info imageObject CLMOBJ 
+| OPMOBJ videoObject CLMOBJ | OPMOBJ imageObject CLMOBJ 
+| OPMOBJ info videoObject genMediaObj CLMOBJ | OPMOBJ info imageObject genMediaObj CLMOBJ 
+| OPMOBJ videoObject genMediaObj CLMOBJ | OPMOBJ imageObject genMediaObj CLMOBJ'''
+
+def p_videoobject(p):
+    '''videoObject : OPVOBJ info videoData CLVOBJ | OPVOBJ videoData CLVOBJ'''
+
+VideoData → <videodata fileref='texto.VideoFormat' />
+
+VideoFormat → MP4 | Ogg | WebM | AVI | MOV
+
+def p_imageobject(p):
+    '''imageobject : OPIMOBJ info imageData CLIMOBJ | OPIMOBJ imageData CLIMOBJ'''
+
+ImageData → <imagedata fileref='texto.ImageFormat' />
+
+ImageFormat → PNG | JPEG | GIF | SVG
+
+def p_genItemizedList(p):
+    '''genItemizedList : listItem | listItem genItemizedList'''
+
+def p_itemizedlist(p):
+    '''itemizedList :  OPILIST genItemizedList CLILIST'''
+
+def p_genListItem(p):
+    '''genListItem : itemizedList | genArt | itemizedList genListItem | genArt genListItem '''
+
+def p_listitem(p):
+    '''listItem : OPLITEM genListItem CLLITEM'''
+
+def p_genTGroup(p):
+    '''genTGroup : tGroup | tGroup genTGroup'''
+
+def p_genInformalTable(p):
+    '''genInformalTable : genMediaObj | genTGroup'''
+
+def p_informaltable(p):
+    '''InformalTable : OPINTAB genInformalTable CLINTAB'''
+
+def p_tgroup(p):
+    '''TGroup : OPTGROUP thead tfoot tbody CLTGROUP | OPTGROUP tfoot tbody CLTGROUP | OPTGROUP thead tbody CLTGROUP | OPTGROUP tbody CLTGROUP'''
+
+def p_genHFB(p):
+    '''genHFB : row | row genHFB'''
+
+def p_thead(p):
+    '''thead : OPHEAD genHFB CLHEAD'''
+
+def p_tfoot(p):
+    '''tfoot : OPFOOT genHFB CLFOOT'''
+
+def p_tbody(p):
+    '''tbody : OPBODY genHFB CLBODY'''
+
+def p_genrow(p):
+    '''genrow : entry | entrytbl | entry genrow | entrytbl genrow'''
+
+def p_row(p):
+    '''row : OPROW genrow CLROW'''
+
+def p_entrytbl(p):
+    '''entrytbl : thead tbody | tbody'''
+
+def p_genEntry(p):
+    '''genEntry : texto | itemizedList | important | para | simPara | mediaObject | comment | abstract 
+    | texto genEntry | itemizedList genEntry | important genEntry | para genEntry | simPara genEntry 
+    | mediaObject genEntry | comment genEntry | abstract genEntry'''
+
+def p_entry(p):
+    '''entry : OPENTRY genEntry CLENTRY'''
+
+LINK →<link xlink:href= PROTOCOLO://DOMINIO_GRAL FINAL_URL </link>
+
+FINAL_URL → /RUTA#LOCALIZADOR | /RUTA
+
+PROTOCOLO → http | https | ftp | ftps
+
+DOMINIO_GRAL → DOMINIO:PUERTO | DOMINIO
+
+LOCALIZADOR → texto
+DOMINIO → texto
+RUTA →texto
+
+
+
+
+
+
 
 
